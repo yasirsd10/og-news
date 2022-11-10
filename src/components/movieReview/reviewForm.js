e3669d927497d7ff4cb53db1ef3a16042c4e29c3
manojb9705 <manojb@banyanspace.com>
2022-11-10 15:26:00 +0530

    added Fomik and Yup for Validations

--- src/components/movieReview/reviewForm.js
+++ src/components/movieReview/reviewForm.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import { FaStar } from "react-icons/fa";
-import { db } from '../../firebase';
+import { db } from "../../firebase";
 import Errorform from "../homeComponents/Errorsform";
 import {
   collection,
@@ -12,7 +12,10 @@ import {
   updateDoc,
   deleteDoc,
   doc,
-} from 'firebase/firestore';
+} from "firebase/firestore";
+import { useHistory } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const colors = {
   orange: "#FFBA5A",
@@ -26,7 +29,6 @@ export default function ReviewForm() {
   const [movieName, setMovieName] = useState("");
   const [movieReview, setMovieReview] = useState("");
   const [showError, setShowError] = useState();
-
 
   const stars = Array(5).fill(0);
 
@@ -42,111 +44,137 @@ export default function ReviewForm() {
     setHoverValue(undefined);
   };
 
-  const createReview = () => {
-    addDoc(collection(db, 'reviews'), {
-      moviePoster: moviePoster,
-      movieName: movieName,
-      movieReview: movieReview });
-      console.log("post successfully");
-        setMovieName('');
-        setMovieReview('');
-        setMoviePoster('');
-  };
-
-  const onSubmitClick = (e) => {
-    e.preventDefault();
-
-    if (moviePoster && movieName && movieReview) {
-      setShowError(false);
-    } else {
-      setShowError(true);
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      moviePoster: "",
+      movieName: "",
+      movieReview: "",
+    },
+    validationSchema: Yup.object({
+      moviePoster: Yup.string().required("Please Provide the Movie Poster URL"),
+      movieName: Yup.string()
+        .max(100, "Movie Name Must Be Less Than 100 Characters")
+        .required("Please Provide the Movie Name"),
+      movieReview: Yup.string()
+        .max(300, "Movie Review Must Be Less Than 300 Characters")
+        .min(10, "Movie Review Must Be 10 Characters or More")
+        .required("Please Provide the Movie Review"),
+    }),
+    onSubmit: (values) => {
+      //   let ref1 = Firebase.database().ref().child('collections').push()
+      // ref1.set(values)
+    },
+  });
 
   return (
-    <form onSubmit={onSubmitClick}>
+    <form onSubmit={formik.handleSubmit}>
       <div style={styles.container} className="mt-5">
-      { showError ? <div >
-          <strong><Errorform  poster = {moviePoster} /></strong>  
-          </div> : null}
-        <label for="basic-url" className ="form-label">
-          Movie Poster URL
-        </label>
-        <div className ="input-group mb-3">
-          <span className ="input-group-text" id="basic-addon3">
-            https://example.com/users/
-          </span>
-          <input
-            type="text"
-            className ="form-control"
-            id="basic-url"
-            aria-describedby="basic-addon3"
-            value={moviePoster}
-            onChange={(e) => {
-              setMoviePoster(e.target.value);
-            }}
-          />
-        </div>
-        <div className ="input-group mb-3">
-          <span className ="input-group-text" id="basic-addon1">
-            Movie Name
-          </span>
-          <input
-            type="text"
-            className ="form-control"
-            placeholder="Movie Name"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            Value={movieName}
-            onChange={(e) => setMovieName(e.target.value)}
-          />
-        </div>
-        <div className="input-group">
-          <span className="input-group-text">Movie Review</span>
-          <textarea
-            className ="form-control"
-            aria-label="With textarea"
-            placeholder="Write A Movie Review"
-            Value={movieReview}
-            onChange={(e) => setMovieReview(e.target.value)}
-          ></textarea>
-        </div>
-        <div className="starsContainer d-flex justify-content-center" style={styles.starsContainer}>
-          <h3> Overal Rating </h3>
-          <div className="d-flex justify-content-center" style={styles.stars}>
-            {stars.map((_, index) => {
-              return (
-                <FaStar
-                  key={index}
-                  size={24}
-                  onClick={() => {
-                    handleClick(index + 1);
-                  }}
-                  onMouseOver={() => handleMouseOver(index + 1)}
-                  onMouseLeave={handleMouseLeave}
-                  color={
-                    (hoverValue || currentValue) > index
-                      ? colors.orange
-                      : colors.grey
-                  }
-                  style={{
-                    marginRight: 10,
-                    cursor: "pointer",
-                  }}
-                />
-              );
-            })}
+        <div className="shadow-sm p-3 mb-5 bg-body rounded ">
+          <div className="mb-1">
+            {formik.touched.moviePoster && formik.errors.moviePoster ? (
+              <div className="errorMessage">{formik.errors.moviePoster}</div>
+            ) : null}
           </div>
-        </div>
-        <div className="d-flex justify-content-center">
-          <button
-          onClick={createReview}
-            className="my-4 btn btn-primary"
-            style={styles.button}
-            type="submit"
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon3">
+              https://example.com/users/
+            </span>
+            <input
+              id="moviePoster"
+              name="moviePoster"
+              type="text"
+              placeholder="Movie Poster URL"
+              class="form-control"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.moviePoster}
+            />
+          </div>
+
+          <div className="mb-1">
+            {formik.touched.movieName && formik.errors.movieName ? (
+              <div className="errorMessage">{formik.errors.movieName}</div>
+            ) : null}
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon3">
+              Movie Name
+            </span>
+            <input
+              id="movieName"
+              name="movieName"
+              type="text"
+              class="form-control"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.movieName}
+            />
+          </div>
+
+          <div className="mb-1">
+            {formik.touched.movieReview && formik.errors.movieReview ? (
+              <div className="errorMessage">{formik.errors.movieReview}</div>
+            ) : null}
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon3">
+              Movie Review
+            </span>
+            <input
+              id="movieReview"
+              name="movieReview"
+              type="movieReview"
+              class="form-control"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.movieReview}
+            />
+          </div>
+          <div
+            className="starsContainer d-flex justify-content-center"
+            style={styles.starsContainer}
           >
-            Submit
-          </button>
+            <h3> Overal Rating </h3>
+            <div className="d-flex justify-content-center" style={styles.stars}>
+              {stars.map((_, index) => {
+                return (
+                  <FaStar
+                    key={index}
+                    size={24}
+                    onClick={() => {
+                      handleClick(index + 1);
+                    }}
+                    onMouseOver={() => handleMouseOver(index + 1)}
+                    onMouseLeave={handleMouseLeave}
+                    color={
+                      (hoverValue || currentValue) > index
+                        ? colors.orange
+                        : colors.grey
+                    }
+                    style={{
+                      marginRight: 10,
+                      cursor: "pointer",
+                    }}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="d-flex justify-content-center">
+            <button
+              className="my-4 btn btn-primary"
+              style={styles.button}
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </form>
